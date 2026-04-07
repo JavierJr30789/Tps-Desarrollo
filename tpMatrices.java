7f790f2f31a16177e3820c56382abf318d7286f2
javierjr30789 <javierjr30789@gmail.com>
2026-04-07 01:06:26 -0300

    ejercicio 1 de matrices terminado

--- tpMatrices.java
+++ tpMatrices.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class tpMatrices {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //declaro la matriz
         int [][] temperaturas  ;
+        //declaro las variables
         int largo, ancho, opcion;
 
 //define el ancho y el largo de la matriz con el scanner
@@ -22,6 +24,8 @@ public class tpMatrices {
             {27, 29, 36, 34},
             {26, 28, 30, 31}
         } ; */
+
+        //usando la estructura do-while para mostrar el menú y ejecutar las opciones hasta que el usuario decida salir
         do {
             System.out.println("Seleccione una opción:");
             System.out.println("1. Cargar matriz");
@@ -30,7 +34,9 @@ public class tpMatrices {
             System.out.println("4. Temperatura mínima");
             System.out.println("5. Temperatura promedio");
             System.out.println("6. Cantidad de temperaturas mayores a una temperatura X");
-            System.out.println("7. Salir");
+            System.out.println("7. Puntos calientes");
+            System.out.println("8. Salir");
+//lee la opción ingresada por el usuario
             opcion = sc.nextInt();
         switch (opcion) {
             case 1 -> cargarMatriz(temperaturas, sc);
@@ -39,15 +45,17 @@ public class tpMatrices {
             case 4 -> temperaturaMinima(temperaturas);
             case 5 -> temperaturaPromedio(temperaturas);
             case 6 -> temperaturaMayor(temperaturas, sc);
-            case 7 -> System.out.println("Saliendo...");
-            default -> throw new AssertionError();
+            case 7 -> puntosCalientes(temperaturas);
+            case 8 -> System.out.println("Saliendo...");
+            // en caso de que el usuario ingrese una opción no válida, se muestra un mensaje de error
+            default -> System.out.println("Opción no válida. Intente nuevamente.");
         }
-} while (opcion != 7);
+} while (opcion != 8);
         
     }
         
     
-    
+    //modulo para mostrar la matriz con formato de tabla
 public static void mostrarMatriz(int[][] temperaturas) {
         for (int i = 0; i < temperaturas.length; i++) {
             System.out.print(" | ");
@@ -57,6 +65,8 @@ public static void mostrarMatriz(int[][] temperaturas) {
             System.out.println();
         }
     }
+
+    //modulo para cargar los valores de la matriz 
     public static void cargarMatriz(int[][] temperaturas, Scanner sc) {
         for (int i = 0; i < temperaturas.length; i++) {
             for (int j = 0; j < temperaturas[i].length; j++) {
@@ -65,6 +75,8 @@ public static void mostrarMatriz(int[][] temperaturas) {
             }
         }
 }
+
+//modulo para calcular la temperatura máxima recorriendo toda la matriz
 public static void temperaturaMaxima(int [][] temperaturas) {
     int max = temperaturas[0][0];
     for (int i = 0; i < temperaturas.length; i++) {
@@ -78,7 +90,7 @@ public static void temperaturaMaxima(int [][] temperaturas) {
 
     
 }
-
+//modulo para calcular la temperatura mínima recorriendo toda la matriz
 public static void temperaturaMinima(int [][] temperaturas) {
     int min = temperaturas[0][0];
     for (int i = 0; i < temperaturas.length; i++) {
@@ -90,7 +102,7 @@ public static void temperaturaMinima(int [][] temperaturas) {
     }
     System.out.println("La temperatura mínima es: " + min);
 }
-
+//modulo para calcular la temperatura promedio recorriendo toda la matriz
 public static void temperaturaPromedio(int [][] temperaturas) {
     int sum = 0;
     int count = 0;
@@ -104,6 +116,7 @@ public static void temperaturaPromedio(int [][] temperaturas) {
     System.out.println("La temperatura promedio es: " + promedio);
 } 
 
+//modulo para calcular la cantidad de temperaturas mayores a una temperatura X ingresada por el usuario recorriendo toda la matriz
 public static void temperaturaMayor(int [][] temperaturas, Scanner sc) {
     System.out.println("ingrese la temperatura a comparar: ");
     int temperaturaX = sc.nextInt();
@@ -116,5 +129,24 @@ public static void temperaturaMayor(int [][] temperaturas, Scanner sc) {
         }
     }
     System.out.println("La cantidad de temperaturas mayores a " + temperaturaX + " es: " + cantidadTemperaturas);
+}
+
+/*modulo para encontrar los puntos calientes, es decir, aquellos valores 
+que son mayores a sus vecinos inmediatos (arriba, abajo, izquierda y derecha) 
+recorriendo toda la matriz*/
+public static void puntosCalientes(int [][] temperaturas) {
+    if (temperaturas.length < 3 || temperaturas[0].length < 3) {
+    System.out.println("La matriz es demasiado pequeña para buscar puntos calientes.");
+    return;
+}
+   for (int i = 1; i < temperaturas.length - 1; i++) {
+        for (int j = 1; j < temperaturas[i].length - 1; j++) {
+            if (temperaturas[i][j] > temperaturas[i-1][j] && temperaturas[i][j] > temperaturas[i+1][j]
+                    && temperaturas[i][j] > temperaturas[i][j-1] && temperaturas[i][j] > temperaturas[i][j+1]) {
+                
+                System.out.println("Punto caliente encontrado en la posición [" + i + "][" + j + "] con valor: " + temperaturas[i][j]);
+            }
+        }
+    }
 }
 }
